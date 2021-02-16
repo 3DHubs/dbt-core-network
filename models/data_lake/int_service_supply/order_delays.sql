dbbb0be68c56ee87c6732ae4e0f7d8da24f2b274
Bart Schuijt <bart.schuijt@3dhubs.com>
2021-02-16 09:04:45 +0000

    first commit

---
+++ models/data_lake/int_service_supply/order_delays.sql
@@ -0,0 +1,9 @@
+select created,
+       updated,
+       deleted,
+       uuid,
+       order_uuid,
+       description,
+       new_shipping_date,
+       reason
+from analytics.int_service_supply.order_delays
