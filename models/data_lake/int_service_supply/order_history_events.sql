cfaa503fd051ff527825f9c329b71abc3388a1cf
bschuijt <56641381+bschuijt@users.noreply.github.com>
2021-02-16 13:00:57 +0100

    Order history events (#2)
    * order history events w/o `extra` and `stack_trace` columns.

---
+++ models/data_lake/int_service_supply/order_history_events.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
+select id,
+       created,
+       order_uuid,
+       quote_uuid,
+       line_item_uuid,
+       user_id,
+       anonymous_id,
+       description
+from int_service_supply.order_history_events
+
+{% if is_incremental() %}
+
+  where id > (select max(id) from {{ this }})
+
+{% endif %}
