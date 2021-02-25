91f77880c0ca69fc67d99ac463fbf6bc1d20a0ca
bschuijt <56641381+bschuijt@users.noreply.github.com>
2021-02-25 11:56:49 +0100

    dim_supplier_technologies reporting table (#18)
    * initial commit
    
    * added tests

---
+++ models/reporting/dim_supplier_technologies.sql
@@ -0,0 +1,17 @@
+select sup.id                                                 as supplier_id,
+       tec.technology_id,
+       tec.name                                               as technology_name,
+       matsub.slug                                            as material,
+       ((supt.min_order_amount::float) / 100)::decimal(15, 2) as min_order_amount_usd,
+       ((supt.max_order_amount::float) / 100)::decimal(15, 2) as max_order_amount_usd,
+       supt.max_active_orders,
+       supt.allow_orders_with_finishes,
+       supt.allow_strategic_orders,
+       supt.allow_non_strategic_orders
+from {{ ref('suppliers') }} as sup
+            left outer join {{ ref('supplier_technologies') }} as supt on sup.id = supt.supplier_id
+            left outer join {{ ref('technologies') }} as tec on supt.technology_id = tec.technology_id
+            left outer join {{ ref('suppliers_material_subsets') }} as supmatsub
+                            on supmatsub.supplier_id = sup.id
+            left outer join {{ ref('material_subsets') }} as matsub
+                            on matsub.material_subset_id = supmatsub.material_subset_id
