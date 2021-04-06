1790bc94cb00955579f98263a6d7a1b8cb8f391e
bschuijt <56641381+bschuijt@users.noreply.github.com>
2021-04-06 16:04:59 +0200

    Mapping data schema fix (#24)
    * all fixed
    
    * Removing freshdesk references for now

--- models/mapping/industry_mapping.sql
+++ models/mapping/industry_mapping.sql
@@ -1,6 +1,13 @@
 /* Industry mapping table depends on the industries.csv file. Be careful, if data is added to industries.csv it may
    change the `industry_id` and `industry_mapped_id` values. So, this may require feature changes in the way we 
    handle this data. E.g. manage the IDs in the csv directly. */
+
+{{
+    config(
+        materialized='table'
+    )
+}}
+
 with unique_industries as (
     select distinct trim(lower(industry_mapped)) as industry_mapped
     from {{ ref('industries') }}
@@ -14,7 +21,7 @@ with unique_industries as (
          select lower(trim(industry))                     as industry,
                 row_number() over (order by industry asc) as industry_id,
                 lower(trim(industry_mapped))              as industry_mapped
-         from temp.industry_mapping
+         from {{ ref('industries') }}
      )
 select tmp.industry,
        tmp.industry_id,
