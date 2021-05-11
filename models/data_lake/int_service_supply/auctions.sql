13bb39c3019d8239188b8a0e269aac1b6fb69f2c
bschuijt <56641381+bschuijt@users.noreply.github.com>
2021-05-11 10:35:10 +0200

    `is_accepted_manually` added (#46)

--- models/data_lake/int_service_supply/auctions.sql
+++ models/data_lake/int_service_supply/auctions.sql
@@ -12,8 +12,8 @@
 with auctions as (select oqs.created,
                          oqs.updated,
                          oqs.deleted,
-                         oqs.order_uuid                                                 as order_uuid,
-                         auctions.uuid                                                  as order_quotes_uuid,
+                         oqs.order_uuid                                                      as order_uuid,
+                         auctions.uuid                                                       as order_quotes_uuid,
                          auctions.winner_bid_uuid,
                          auctions.status,                       -- If auction gets status 'resourced' it means it has been brought back to the auction
                          auctions.started_at,
@@ -22,14 +22,15 @@ with auctions as (select oqs.created,
                          auctions.last_processed_at,
                          auctions.internal_support_ticket_id,
                          decode(auctions.is_internal_support_ticket_opened, 'true', True, 'false',
-                                False)                                                  as is_internal_support_ticket_opened,
-                         decode(auctions.china_throttled, 'true', True, 'false', False) as is_china_throttled,
+                                False)                                                       as is_internal_support_ticket_opened,
+                         decode(auctions.china_throttled, 'true', True, 'false', False)      as is_china_throttled,
                          auctions.base_margin,                  -- For debugging purposes only, do not use for reporting
                          auctions.base_margin_without_discount, -- This field will be used in auctions
                          auctions.next_allocate_at,
-                         decode(auctions.is_resourcing, 'true', True, 'false', False)   as is_resourcing,
+                         decode(auctions.is_accepted_manually, 'true', True, 'false', False) as is_accepted_manually,
+                         decode(auctions.is_resourcing, 'true', True, 'false', False)        as is_resourcing,
                          row_number() over (partition by oqs.order_uuid order by auctions.started_at desc nulls last)
-                                                                                        as recency_idx
+                                                                                             as recency_idx
                   from int_service_supply.auctions as auctions
                            inner join {{ ref('cnc_order_quotes') }} as oqs
                                       on auctions.uuid = oqs.uuid
