b74e1891992b772d2c06cf7bbbc91048d189c824
Jurien Groot <jurien@hubs.com>
2021-06-30 03:54:46 +0000

    Fix discrepancies

--- models/data_lake/int_service_supply/supplier_auctions.sql
+++ models/data_lake/int_service_supply/supplier_auctions.sql
@@ -27,6 +27,7 @@ select supplier_id,
        estimated_customs_rate,
        ship_by_date,
        shipping_added_lead_time,
+       is_preferred_auction,
        {% for boolean_field in boolean_fields %}
            {{ varchar_to_boolean(boolean_field) }}
            {% if not loop.last %},{% endif %}
