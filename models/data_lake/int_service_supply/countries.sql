7c41e583796963968219f71914f19d363eb51997
nihadazimli <nihadazimli@outlook.com>
2021-07-06 16:22:57 +0400

    PTECH-701 `agg_google_search_console` (#90)
    * migration for gsc aggregation from SP to dbt
    
    * Update models/reporting/agg_google_search_console.sql
    
    Co-authored-by: bschuijt <56641381+bschuijt@users.noreply.github.com>
    
    * changed problematic part with incremental changes
    
    Co-authored-by: Bart Schuijt <bart.schuijt@3dhubs.com>
    Co-authored-by: bschuijt <56641381+bschuijt@users.noreply.github.com>

--- models/data_lake/int_service_supply/countries.sql
+++ models/data_lake/int_service_supply/countries.sql
@@ -5,32 +5,37 @@
     ]
 %}
 
-select created,
-       updated,
-       deleted,
-       country_id,
-       name,
-       alpha2_code,
-       continent,
-       currency_code,
-       coordinates,
-       lat,
-       lon,
+select issc.created,
+       issc.updated,
+       issc.deleted,
+       issc.country_id,
+       issc.name,
+       issc.alpha2_code,
+       issc.continent,
+       issc.currency_code,
+       issc.coordinates,
+       issc.lat,
+       issc.lon,
        --region
-       case when lower(alpha2_code) in ('al','ad','by','dk','ee','fi','ax','at','be','ba','bg','es-cn','hr','cy','cz','fo','fr','de','gi','gr','hu','ie','je','lv','lt','mk','mt','md','mc','no','pt','se','ch','ua','gg','is','im','it','li','lu','me','nl','pl','ro','ru','sm','rs','sk','si','es','sj','gb','va') then 'europe'
-            when lower(alpha2_code) in ('ai','aw','bs','bb','cw','do','ag','bz','bm','bq','vg','ca','ky','cr','cu','dm','sv','gl','gp','mq','ms','an','pr','mf','pm','sx','tc','gd','gt','ht','hn','jm','mx','ni','pa','bl','kn','lc','vc','tt','vi','us') then 'north-america'
+       case when lower(issc.alpha2_code) in ('al','ad','by','dk','ee','fi','ax','at','be','ba','bg','es-cn','hr','cy','cz','fo','fr','de','gi','gr','hu','ie','je','lv','lt','mk','mt','md','mc','no','pt','se','ch','ua','gg','is','im','it','li','lu','me','nl','pl','ro','ru','sm','rs','sk','si','es','sj','gb','va') then 'europe'
+            when lower(issc.alpha2_code) in ('ai','aw','bs','bb','cw','do','ag','bz','bm','bq','vg','ca','ky','cr','cu','dm','sv','gl','gp','mq','ms','an','pr','mf','pm','sx','tc','gd','gt','ht','hn','jm','mx','ni','pa','bl','kn','lc','vc','tt','vi','us') then 'north-america'
             else 'row'
             end region,
        --market
-       case when lower(alpha2_code) in ('dk','fi','be','no','se','lu','nl') then 'benelux and nordics'
-            when lower(alpha2_code) in ('at','de','ch') then 'dach'
-            when lower(alpha2_code) = 'fr' then 'france'
-            when lower(alpha2_code) in ('ie','gb') then 'uki'
-            when lower(alpha2_code) in ('ca','us') then 'us/ca'
+       case when lower(issc.alpha2_code) in ('dk','fi','be','no','se','lu','nl') then 'benelux and nordics'
+            when lower(issc.alpha2_code) in ('at','de','ch') then 'dach'
+            when lower(issc.alpha2_code) = 'fr' then 'france'
+            when lower(issc.alpha2_code) in ('ie','gb') then 'uki'
+            when lower(issc.alpha2_code) in ('ca','us') then 'us/ca'
             else 'other'
             end market,
+            scmm.country_iso3,
        {% for boolean_field in boolean_fields %}
            {{ varchar_to_boolean(boolean_field) }}
            {% if not loop.last %},{% endif %}
        {% endfor %}
-from int_service_supply.countries
+from int_service_supply.countries as issc
+left join {{ source('data_lake', 'supply_countries_markets_mapping')}} as scmm on lower(issc.alpha2_code) = scmm.country_iso2
+
+
+
---
+++ models/reporting/agg_google_search_console.sql
@@ -0,0 +1,57 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
+with stg_gsc_data as (
+    select date,
+           case when keys ~ 'https.{3}www.3dhubs.com' then '3dhubs' else 'hubs' end tld,
+           translate(keys, '[]', '')                                 as keys_clean,
+           split_part(regexp_replace(trim(translate(split_part(keys_clean, ',', 1), '''', '')), 'https://www.{1,3}hubs.com', ''), '#', 1) -- Catering for 3dhubs.com and hubs.com
+                                                                     as url,
+           trim(translate(split_part(keys_clean, ',', 2), '''', '')) as country_iso3,
+           trim(translate(split_part(keys_clean, ',', 3), '''', '')) as keywords,
+           position::decimal(15, 1)                                  as position,
+           impressions,
+           clicks
+    from {{ source('data_lake', 'google_search_console') }}
+    where true
+      and dimension1 = 'page'
+      and dimension2 = 'country'
+      and dimension3 = 'query'
+
+{% if is_incremental() %}
+
+  and date > (select max(date) from {{ this }})
+
+{% endif %}
+),
+stg_join_w_country as (
+    select gsc.date,
+           gsc.tld,
+           gsc.keys_clean,
+           gsc.url,
+           case when len(split_part(url, '/', 2)) = 2 then split_part(url, '/', 2) else 'en' end as language,
+           gsc.country_iso3                      as country_code,
+           gsc.keywords,
+           coalesce(spg.page_group, 'Ungrouped') as page_group,
+           gsc.position,
+           gsc.impressions,
+           gsc.clicks,
+           scmm.market,
+           scmm.name                             as country,
+           gsc.url || gsc.keywords               as target_id
+    from stg_gsc_data as gsc
+             left join {{ref('countries_mapping')}} as scmm on scmm.country_iso3 = gsc.country_iso3
+             left join {{ref('seo_page_groups')}} as spg on spg.page = gsc.url
+),
+stg_join_w_seo_targets as (
+    select gsc_country.*,
+           case when gsct.target_id is not null then true else false end is_seo_target
+    from stg_join_w_country as gsc_country
+             left join {{source('data_lake', 'static_seo_targets')}} as gsct on gsct.target_id = gsc_country.target_id
+)
+
+select *
+from stg_join_w_seo_targets
