93ad48c433f3e44390fe6290299bfd573b00746b
bschuijt <56641381+bschuijt@users.noreply.github.com>
2021-07-07 14:00:51 +0200

    PTECH-739 fix schema for google & bing models (#102)
    * fix schema for google & bing models
    
    * address `invalid reference to FROM-clause` in join clause

--- models/external/bing_ads/bing_ads_campaign_performance_report.sql
+++ models/external/bing_ads/bing_ads_campaign_performance_report.sql
@@ -7,36 +7,48 @@
 }}
 
 with campaign_performance_report_ranked as
+(
+    select
+        *,
+        row_number() over (
+            partition by
+                     timeperiod, campaignid, customerid, devicetype
+            order by _sdc_report_datetime desc
+        ) as row_number
+
+    from {{ source('ext_bing', 'campaign_performance_report') }}
+-- Stitch does append-only loading of data for the report tables. Since each report is updated every day for 30 days after the the day, we need to select the latest record only
+)
+
+select
+    customerid::bigint as account_id,
+    accountname as account_name,
+    allconversions as all_conversions,
+    campaignname as campaign_name,
+    campaignid as campaign_id,
+    clicks,
+    devicetype as device,
+    conversions,
+    spend::decimal(9, 2) as cost_orginal_currency,
+    currencycode as orginal_currency,
+    timeperiod as date,
+    impressions,
+    campaignlabels as labels,
+    absolutetopimpressionsharepercent as seach_absolute_top_impression_share,
+    impressionsharepercent as seach_impression_share,
+    topimpressionsharepercent as search_top_impression_share,
+    viewthroughconversions as view_trough_conversions,
     (
-        select *,
-                row_number() over (partition by timeperiod, campaignid, customerid, devicetype order by _sdc_report_datetime desc) as row_number
-
-        from {{ source('ext_bing', 'campaign_performance_report') }}
-    ) -- Stitch does append-only loading of data for the report tables. Since each report is updated every day for 30 days after the the day, we need to select the latest record only
-
-select customerid::bigint as account_id,
-        accountname as account_name,
-        allconversions as all_conversions,
-        campaignname as campaign_name,
-        campaignid as campaign_id,
-        clicks,
-        devicetype as device,
-        conversions,
-        spend::decimal(9,2) as cost_orginal_currency,
-        (cost_orginal_currency / rates.rate)::decimal(9,2) as cost_usd,
-        currencycode as orginal_currency,
-        timeperiod as date,
-        impressions,
-        campaignlabels as labels,
-        absolutetopimpressionsharepercent as seach_absolute_top_impression_share,
-        impressionsharepercent as seach_impression_share,
-        topimpressionsharepercent as search_top_impression_share,
-        viewthroughconversions as view_trough_conversions
+        cost_orginal_currency / exchange_rate_spot_daily.rate
+    )::decimal(9, 2) as cost_usd
 
 from campaign_performance_report_ranked
 
-left join analytics.data_lake.exchange_rate_spot_daily as rates
-    on rates.currency_code_to = campaign_performance_report_ranked.currencycode
-    and trunc(campaign_performance_report_ranked.timeperiod) = trunc(rates.date)
+left join {{ source('data_lake', 'exchange_rate_spot_daily') }} as exchange_rate_spot_daily
+    on
+        exchange_rate_spot_daily.currency_code_to = campaign_performance_report_ranked.currencycode
+        and trunc(
+            campaign_performance_report_ranked.timeperiod
+        ) = trunc(exchange_rate_spot_daily.date)
 
 where row_number = 1
--- models/external/bing_ads/bing_ads_keywords_performance_report.sql
+++ models/external/bing_ads/bing_ads_keywords_performance_report.sql
@@ -7,51 +7,81 @@
 }}
 
 with keywords_performance_report_ranked as
+(
+    select
+        *,
+        row_number() over (
+            partition by
+                     timeperiod, keywordid, adgroupid, accountid
+            order by _sdc_report_datetime desc
+        ) as row_number
+    from {{ source('ext_bing', 'keyword_performance_report') }}
+)
+
+select
+    accountid::bigint as account_id,
+    accountname as account_name,
+    accountnumber as account_number,
+    accountstatus as account_status,
+    adgroupid as adgroup_id,
+    adgroupname as adgroup_name,
+    adgroupstatus as adgroup_status,
+    allconversions as all_conversions,
+    averageposition as average_position,
+    campaignid as campaign_id,
+    campaignname as campaign_name,
+    campaignstatus as campaign_status,
+    clicks,
+    conversions,
+    spend::decimal(9, 2) as cost_orginal_currency,
+    currencycode as orginal_currency,
+    currentmaxcpc as current_max_cpc_orginal_currency,
+    impressions,
+    keyword,
+    keywordid as keyword_id,
+    keywordlabels as labels,
+    keywordstatus as keyword_status,
+    landingpageexperience as landingpage_experience,
+    qualityscore as quality_score,
+    timeperiod as date,
+    viewthroughconversions as view_trough_conversions,
+    mainlinebid as top_of_page_cpc_orginal_currency,
+    mainline1bid as firstposition_cpc_orginal_currency,
     (
-        select *,
-                row_number() over (partition by timeperiod, keywordid, adgroupid, accountid order by _sdc_report_datetime desc) as row_number
-        from ext_bing.keyword_performance_report
-    ) 
-select accountid::bigint as account_id,
-        accountname as account_name,
-        accountnumber as account_number,
-        accountstatus as account_status,
-        adgroupid as adgroup_id,
-        adgroupname as adgroup_name,
-        adgroupstatus as adgroup_status,
-        allconversions as all_conversions,
-        averageposition as average_position,
-        campaignid as campaign_id,
-        campaignname as campaign_name,
-        campaignstatus as campaign_status,
-        clicks,
-        conversions,
-        spend::decimal(9,2) as cost_orginal_currency,
-        (cost_orginal_currency / rates.rate)::decimal(9,2) as cost_usd,
-        currencycode as orginal_currency,
-        currentmaxcpc as current_max_cpc_orginal_currency,
-        (current_max_cpc_orginal_currency / rates.rate)::decimal(9,2) as current_max_cpc_usd,
-        nullif(historicaladrelevance, '--')::int as historical_ad_relevance,
-        nullif(historicalexpectedctr, '--')::int as historical_expected_ctr,
-        nullif(historicallandingpageexperience, '--')::int as historical_landingpage_experience,
-        nullif(historicalqualityscore, '--')::int as historical_quality_score,
-        impressions,
-        keyword,
-        keywordid as keyword_id,
-        keywordlabels as labels,
-        keywordstatus as keyword_status,
-        landingpageexperience as landingpage_experience,
-        qualityscore as quality_score,
-        timeperiod as date,
-        viewthroughconversions as view_trough_conversions,
-        mainlinebid as top_of_page_cpc_orginal_currency,
-        (top_of_page_cpc_orginal_currency / rates.rate)::decimal(9,2) as top_of_page_cpc_usd,
-        mainline1bid as firstposition_cpc_orginal_currency,
-        (firstposition_cpc_orginal_currency / rates.rate)::decimal(9,2) as firstposition_cpc_usd,
-        nullif(firstpagebid,'--')::double precision as firstpage_cpc_orginal_currency,
-        (firstpage_cpc_orginal_currency / rates.rate)::decimal(9,2) as firstpage_cpc_usd
+        cost_orginal_currency / data_lake_exchange_rate_spot_daily.rate
+    )::decimal(9, 2) as cost_usd,
+    (
+        current_max_cpc_orginal_currency / data_lake_exchange_rate_spot_daily.rate
+    )::decimal(9, 2) as current_max_cpc_usd,
+    nullif(
+        historicaladrelevance, '--'
+    )::int as historical_ad_relevance,
+    nullif(
+        historicalexpectedctr, '--'
+    )::int as historical_expected_ctr,
+    nullif(
+        historicallandingpageexperience, '--'
+    )::int as historical_landingpage_experience,
+    nullif(
+        historicalqualityscore, '--'
+    )::int as historical_quality_score,
+    (
+        top_of_page_cpc_orginal_currency / data_lake_exchange_rate_spot_daily.rate
+    )::decimal(9, 2) as top_of_page_cpc_usd,
+    (
+        firstposition_cpc_orginal_currency / data_lake_exchange_rate_spot_daily.rate
+    )::decimal(9, 2) as firstposition_cpc_usd,
+    nullif(firstpagebid, '--')::double precision as firstpage_cpc_orginal_currency,
+    (
+        firstpage_cpc_orginal_currency / data_lake_exchange_rate_spot_daily.rate
+    )::decimal(9, 2) as firstpage_cpc_usd
+
 from keywords_performance_report_ranked
-left join {{ source('data_lake', 'exchange_rate_spot_daily') }} as rates
-    on rates.currency_code_to = keywords_performance_report_ranked.currencycode
-    and trunc(keywords_performance_report_ranked.timeperiod) = trunc(rates.date)
+
+left join {{ source('data_lake', 'exchange_rate_spot_daily') }} as data_lake_exchange_rate_spot_daily
+    on
+        data_lake_exchange_rate_spot_daily.currency_code_to = keywords_performance_report_ranked.currencycode
+        and trunc(
+            keywords_performance_report_ranked.timeperiod
+        ) = trunc(data_lake_exchange_rate_spot_daily.date)
 where row_number = 1
