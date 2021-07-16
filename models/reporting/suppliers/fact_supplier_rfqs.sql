ab9a9c40c0799cbd42995cfd7e2561f971e44a79
tashbukholt <57940436+tashbukholt@users.noreply.github.com>
2021-07-16 14:09:49 +0200

    `fact_supplier_rfqs` supports multiple RFQs per order (#111)
    * refactor fact_supplier_rfqs
    * changed supplier_rfq_id to supplier_rfq_uuid
    
    Co-authored-by: Natasha Bukholt <natasha@hubs.com>
    Co-authored-by: bart.schuijt@3dhubs.com <bart.schuijt@3dhubs.com>

--- models/reporting/suppliers/fact_supplier_rfqs.sql
+++ models/reporting/suppliers/fact_supplier_rfqs.sql
@@ -1,59 +1,103 @@
 with winning_bid as (
-        select oqsl.parent_uuid as uuid
+    select oqsl.parent_uuid as uuid
 
-        from {{ ref('cnc_order_quotes') }} oqsl
-            inner join  {{ ref('purchase_orders') }} spocl
-                on oqsl.uuid = spocl.uuid
+    from {{ ref('cnc_order_quotes') }} oqsl
+             inner join {{ ref('purchase_orders') }} spocl
+                        on oqsl.uuid = spocl.uuid
 
-        where oqsl.type = 'purchase_order'
-            and spocl.status = 'active'
+    where oqsl.type = 'purchase_order'
+      and spocl.status = 'active'
 
-        group by 1
-     ),
-     -- for each order it is only possible to have 1 RFQ sent per supplier
-     -- however it is possible for an admin to duplicate a quote that has been RFQed, which creates a new RFQ auction (as well as bids)
-     -- example order_uuid = 00189119-d4e6-4c79-bed9-7969956f09bf
-     -- we only want to consider the first RFQ bid response for each supplier so create bid_index here
-     supplier_rfq_bids as (
+    group by 1
+),
+    supplier_rfq_bids as (
         select bid_quotes.*,
-               bids.placed_at,
-               bids.uuid                                                                              as bid_uuid,
-               bids.supplier_id,
-               row_number()
-               over (partition by bid_quotes.order_uuid, supplier_id order by bid_quotes.created asc) as supplier_bid_idx
-        
-        from {{ ref('cnc_order_quotes') }} as bid_quotes
-            left outer join {{ ref('bids') }} as bids
-                on bid_quotes.uuid = bids.uuid
-            left outer join {{ ref('cnc_order_quotes') }} as auction_quote
-                on auction_quote.uuid = bid_quotes.parent_uuid
-            inner join {{ ref('supplier_rfqs_src_auction') }} as auctions
-                on auctions.order_quotes_uuid = auction_quote.uuid
-        
+            bids.placed_at,
+            bids.uuid                                                                              as bid_uuid,
+            bids.supplier_id,
+            md5(bids.supplier_id || bids.auction_uuid)                                             as supplier_rfq_uuid,
+            row_number()
+            over (partition by bid_quotes.order_uuid, supplier_id order by bid_quotes.created asc) as supplier_bid_idx
+        from  {{ ref('cnc_order_quotes') }} as bid_quotes
+                left outer join {{ ref('bids') }} as bids
+                                on bid_quotes.uuid = bids.uuid
+                left outer join {{ ref('cnc_order_quotes') }} as auction_quote
+                                on auction_quote.uuid = bid_quotes.parent_uuid
+                inner join {{ ref('supplier_rfqs_src_auction') }} as auctions
+                            on auctions.order_quotes_uuid = auction_quote.uuid
         where bid_quotes.type = 'bid'
-     )
+    ),
+    --query all supplier auctions that were RFQ'ed. One order can have multiple auctions per suppliers
+    supplier_rfq_auctions as (
+        select md5(sa.supplier_id || sa.auction_uuid) as supplier_rfq_uuid,
+            assigned_at                            as rfq_sent_date,
+            supplier_id,
+            order_uuid,
+            auction_uuid
+        from {{ ref('supplier_auctions') }} as sa
+                inner join {{ ref('supplier_rfqs_src_auction') }} sr on sr.order_quotes_uuid = sa.auction_uuid
+    ),
+    --query all supplier auctions that were RFQ'ed, as well as the response from suppliers
+    supplier_rfq_auction_interactions as (
+        select supplier_rfq_auctions.supplier_rfq_uuid                                              as supplier_rfq_uuid,
+            supplier_rfq_auctions.order_uuid,
+            supplier_rfq_auctions.supplier_id,
+            s.name                                                                               as supplier_name,
+            supplier_rfq_auctions.rfq_sent_date                                                  as supplier_rfq_sent_date,
+            bid_quotes.lead_time,
+            round(bid_quotes.subtotal_price_amount / 100.00, 2)                                  as rfq_bid_amount,
+            bid_quotes.currency_code                                                             as rfq_bid_amount_currency,
+            round(((bid_quotes.subtotal_price_amount / 100.00) / rates.rate), 2)::decimal(15, 2) as rfq_bid_amount_usd,
+            case when winning_bid.uuid > 0 then true else false end                              as is_winning_bid,
+            bid_quotes.placed_at                                                                 as supplier_rfq_responded_date
 
-select supplier_rfqs.id                                                                     as supplier_rfq_id,
-       supplier_rfqs.order_uuid,
-       supplier_rfqs.supplier_id,
-       s.name                                                                               as supplier_name,
-       supplier_rfqs.created                                                                as supplier_rfq_sent_date,
-       bid_quotes.lead_time,
-       round(bid_quotes.subtotal_price_amount / 100.00, 2)                                  as rfq_bid_amount,
-       bid_quotes.currency_code                                                             as rfq_bid_amount_currency,
-       round(((bid_quotes.subtotal_price_amount / 100.00) / rates.rate), 2)::decimal(15, 2) as rfq_bid_amount_usd,
-       case when winning_bid.uuid > 0 then true else false end                              as is_winning_bid,
-       bid_quotes.placed_at                                                                 as supplier_rfq_responded_date
+        from supplier_rfq_auctions
+                left outer join supplier_rfq_bids as bid_quotes
+                                on supplier_rfq_auctions.supplier_rfq_uuid = bid_quotes.supplier_rfq_uuid
+                left outer join {{ ref('suppliers') }} as s
+                                on s.id = supplier_rfq_auctions.supplier_id
+                left outer join {{ source('data_lake', 'exchange_rate_spot_daily') }} as rates
+                                on rates.currency_code_to = bid_quotes.currency_code
+                                    and trunc(bid_quotes.created) = trunc(rates.date)
+                left outer join winning_bid
+                                on bid_quotes.bid_uuid = winning_bid.uuid
+    ),
+    --query old supplier RFQs - these are orders that were RFQ'ed before we automated the RFQ process (and also started creating an auction for supplier RFQs)
+    --supplier_rfqs only allows 1 RFQ request per supplier (per order) when in reality an order can have mulitple RFQ requests from the same order
+    --so this is the supplier RFQ table is only used to collect historical data
+    supplier_rfqs as (
+        select supplier_rfqs.id::varchar                                                            as supplier_rfq_uuid,
+            supplier_rfqs.order_uuid,
+            supplier_rfqs.supplier_id,
+            s.name                                                                               as supplier_name,
+            supplier_rfqs.created                                                                as supplier_rfq_sent_date,
+            bid_quotes.lead_time,
+            round(bid_quotes.subtotal_price_amount / 100.00, 2)                                  as rfq_bid_amount,
+            bid_quotes.currency_code                                                             as rfq_bid_amount_currency,
+            round(((bid_quotes.subtotal_price_amount / 100.00) / rates.rate), 2)::decimal(15, 2) as rfq_bid_amount_usd,
+            case when winning_bid.uuid > 0 then true else false end                              as is_winning_bid,
+            bid_quotes.placed_at                                                                 as supplier_rfq_responded_date
 
-from {{ ref('supplier_rfqs') }} as supplier_rfqs
-    left outer join supplier_rfq_bids as bid_quotes
-        on supplier_rfqs.order_uuid = bid_quotes.order_uuid
-        and bid_quotes.supplier_id = supplier_rfqs.supplier_id
-        and supplier_bid_idx = 1
-    left outer join {{ ref('suppliers') }} as s 
-        on s.id = supplier_rfqs.supplier_id
-    left outer join {{ source('data_lake', 'exchange_rate_spot_daily') }} as rates
-        on rates.currency_code_to = bid_quotes.currency_code
-        and trunc(bid_quotes.created) = trunc(rates.date)
-    left outer join winning_bid
-        on bid_quotes.bid_uuid = winning_bid.uuid
+        from  {{ ref('supplier_rfqs') }} as supplier_rfqs
+                left outer join supplier_rfq_bids as bid_quotes
+                                on supplier_rfqs.order_uuid = bid_quotes.order_uuid
+                                    and bid_quotes.supplier_id = supplier_rfqs.supplier_id
+                                    and supplier_bid_idx = 1
+                left outer join {{ ref('suppliers') }} as s
+                                on s.id = supplier_rfqs.supplier_id
+                left outer join {{ source('data_lake', 'exchange_rate_spot_daily') }} as rates
+                                on rates.currency_code_to = bid_quotes.currency_code
+                                    and trunc(bid_quotes.created) = trunc(rates.date)
+                left outer join winning_bid
+                                on bid_quotes.bid_uuid = winning_bid.uuid
+    )
+
+--union rfq supplier auctions and supplier rfqs to get an overview of all RFQs sent 
+select *
+from supplier_rfq_auction_interactions
+
+union all
+
+select *
+from supplier_rfqs
+where order_uuid not in (select order_uuid from supplier_rfq_auction_interactions)
