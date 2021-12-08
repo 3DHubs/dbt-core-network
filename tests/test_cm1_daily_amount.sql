5fe1503bc2df36e1c25e85546b6145a695e6cbd4
Dataniel Salazar <85174516+Daniel-SalazarSoplapuco@users.noreply.github.com>
2021-12-08 15:38:29 +0100

    split the cm1 test in recognized revenue and cogs (#225)

--- tests/test_cm1_daily_amount.sql
+++ tests/test_cm1_daily_amount.sql
@@ -1,10 +1,10 @@
 select dd.date,
-       sum(coalesce(amount, 0)) total_amount
-from {{ source('data_lake', 'dim_dates') }} dd
-left join (select recognized_date as date, sum(amount_usd) as amount
-    from{{ ref('fact_contribution_margin' )}} group by 1 ) rev
+        sum(case when type = 'cost' then coalesce(amount, 0) else 0 end) as recognized_cost,
+        sum(case when type = 'revenue' then coalesce(amount, 0) else 0 end) as recognized_revenue
+from data_lake.dim_dates dd
+left join (select recognized_date as date, type as type, sum(amount_usd) as amount
+    from dbt_prod_reporting.fact_contribution_margin group by 1, 2) rev
 on rev.date = dd.date
 where dd.date = dateadd(day, -1, getdate()::date) and extract(dayofweek from getdate()) not in (6,0)
 group by 1
-having not (total_amount > 0)
-
+having not (recognized_cost < 0 or recognized_revenue > 0)
