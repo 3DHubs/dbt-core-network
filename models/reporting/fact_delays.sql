fb93076276ee89445a4771182abb3f320bf79772
Diego <61149777+diegocamelo92@users.noreply.github.com>
2022-01-12 09:01:02 +0100

    Removed Intermediate Table +Added Comment (#237)
    Co-authored-by: Diego Camelo <diego@hubs.com>

--- models/reporting/fact_delays.sql
+++ models/reporting/fact_delays.sql
@@ -1,24 +1,24 @@
-{{
-    config(
-        pre_hook = "analyze {{ ref('order_delays') }}",
-        post_hook = "analyse {{ this }}"
-    )
-}}
-
 /*
  * This model aggregates data on `created`, `uuid`,
  * and `description` to guarantee uniqueness on these
  * attributes as there are some duplicate delay
  * entries. Note that `uuid` is a random value in case
  * there are duplicate issues but that should not
- * affect the data.
+ * affect the data. 
  */
+
+
+-- Update: January 2022 (Diego)
+-- This data is currently being analysed at the order level but in the
+-- future with the new setup of shipments/packages and batch shipments
+-- we might want to reconsider checking this at the package level. 
+
 select min(created) as  submitted_at,
        max(uuid)    as  delay_uuid,
-       order_uuid,
        max(description) delay_description,
+       order_uuid,
        new_shipping_date,
        reason       as  delay_category
-from {{ ref('order_delays') }}
+from {{ source('int_service_supply', 'order_delays') }}
 where deleted is null
-group by 3, 5, 6
+group by 4, 5, 6
