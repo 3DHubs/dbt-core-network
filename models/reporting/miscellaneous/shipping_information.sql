7ef28ff3a80a3f9021c432967afdaa4a14ac52e0
Daniel Salazar Soplapuco <85174516+Daniel-SalazarSoplapuco@users.noreply.github.com>
2022-02-02 18:40:57 +0100

    Ace 823 phase out shipping information model to dbt (#252)
    * Removing unnecessary fields
    
    * Remove unnecessary changes
    
    * Remove declared value from total costs

--- models/reporting/miscellaneous/shipping_information.sql
+++ models/reporting/miscellaneous/shipping_information.sql
@@ -1,27 +1,21 @@
 select
     sci.carrier,
-    sci.awb,
-    sci.declared_value,
-    sci.custom_outlay,
-    sci.shipping_cost,
-    sci.vat,
-    sci.other_costs,
     sci.carrier_invoiced_date,
-    sci.entity,
     sci.carrier_invoice_number,
+    sci.awb,
+    sci.entity,
     sci.finance_invoiced_date,
     sci.quote_document_number,
     sci.currency,
     sci.weight_kg,
     oqsl.order_uuid,
     coalesce(ship.shipping_leg, sci.shipping_leg) as shipping_leg_full,
-    declared_value + custom_outlay + shipping_cost + vat + other_costs as total_costs,
-    coalesce(round(declared_value / ex.rate,2),0) as declared_value_usd,
-    coalesce(round(custom_outlay / ex.rate,2),0) as custom_outlay_usd,
-    coalesce(round(shipping_cost / ex.rate,2),0) as shipping_cost_usd,
-    coalesce(round(vat / ex.rate,2),0) as vat_usd,
-    coalesce(round(other_costs / ex.rate,2),0) as other_costs_usd,
-    declared_value_usd + custom_outlay_usd + shipping_cost_usd + vat_usd + other_costs_usd as total_costs_usd
+    coalesce(round(sci.declared_value / ex.rate,2),0) as declared_value_usd,
+    coalesce(round(sci.custom_outlay / ex.rate,2),0) as custom_outlay_usd,
+    coalesce(round(sci.shipping_cost / ex.rate,2),0) as shipping_cost_usd,
+    coalesce(round(sci.vat / ex.rate,2),0) as vat_usd,
+    coalesce(round(sci.other_costs / ex.rate,2),0) as other_costs_usd,
+    custom_outlay_usd + shipping_cost_usd + vat_usd + other_costs_usd as total_costs_usd
 from {{ source('int_logistics', 'shipping_customs_information') }} as sci
 left join {{ source('int_service_supply', 'cnc_order_quotes') }} as oqsl on sci.quote_document_number = oqsl.document_number
 left join {{ source('int_service_supply', 'shipments') }} as ship on ship.tracking_number = sci.awb
