b1a5642d187deb647d5e24ca83eba1b31a8b0ad4
Daniel Salazar Soplapuco <85174516+Daniel-SalazarSoplapuco@users.noreply.github.com>
2022-04-21 16:44:25 +0200

    Manual Refresh Branch For Shipping Information (#303)
    * moving new fact_shipment_information model to master branch due to manual refresh tag
    
    * removing old table int_logistics.shipping_customs_information from shema
    
    * adding shipper_postal_code
    
    * adding service level category

--- models/4_reporting/logistics/fact_shipping_information.sql
+++ models/4_reporting/logistics/fact_shipping_information.sql
@@ -3,25 +3,28 @@
 ) }}
 
 select
-    sci.carrier,
-    sci.carrier_invoiced_date,
-    sci.carrier_invoice_number,
-    sci.awb,
-    sci.entity,
-    sci.finance_invoiced_date,
-    sci.quote_document_number,
-    sci.currency,
-    sci.weight_kg,
     oqsl.order_uuid,
+    sci.carrier,
+    sci.invoice_date,
+    sci.shipment_date,
+    sci.product_name,
+    sci.service_category,
+    sci.invoice_number,
+    sci.shipment_number,
+    sci.corporate_country,
+    sci.document_number,
+    sci.weight_kg,
+    sci.recipient_postal_code,
+    sci.shipper_postal_code,
+    coalesce(round(sci.statistical_value/ ex.rate,2),0) as statistical_value,
     coalesce(ship.shipping_leg, sci.shipping_leg) as shipping_leg_full,
-    coalesce(round(sci.declared_value / ex.rate,2),0) as declared_value_usd,
-    coalesce(round(sci.custom_outlay / ex.rate,2),0) as custom_outlay_usd,
-    coalesce(round(sci.shipping_cost / ex.rate,2),0) as shipping_cost_usd,
-    coalesce(round(sci.vat / ex.rate,2),0) as vat_usd,
-    coalesce(round(sci.other_costs / ex.rate,2),0) as other_costs_usd,
-    custom_outlay_usd + shipping_cost_usd + vat_usd + other_costs_usd as total_costs_usd
-from {{ source('int_logistics', 'shipping_customs_information') }} as sci
-left join {{ source('int_service_supply', 'cnc_order_quotes') }} as oqsl on sci.quote_document_number = oqsl.document_number
-left join {{ source('int_service_supply', 'shipments') }} as ship on ship.tracking_number = sci.awb
-left join {{ source('data_lake', 'exchange_rate_spot_daily') }} as ex on sci.currency = ex.currency_code_to and sci.carrier_invoiced_date = trunc(ex.date)
+    sci.charge_name,
+    sci.charge_category,
+    sci.currency,
+    coalesce(round(sci.charge_amount / ex.rate,2),0) as charge_amount,
+    coalesce(round(sci.total_amount / ex.rate,2),0) as total_costs_usd
+from {{ source('int_logistics', 'automated_shipping_customs_information') }} as sci
+left join {{ source('int_service_supply', 'cnc_order_quotes') }} as oqsl on sci.document_number = oqsl.document_number
+left join {{ source('int_service_supply', 'shipments') }} as ship on ship.tracking_number = sci.shipment_number
+left join {{ source('data_lake', 'exchange_rate_spot_daily') }} as ex on sci.currency = ex.currency_code_to and sci.invoice_date = trunc(ex.date)
 where oqsl.type = 'quote'
