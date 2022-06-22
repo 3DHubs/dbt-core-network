c1bf949c88ce07a1140bd5926ccdd9277856eb8d
Diego <61149777+diegocamelo92@users.noreply.github.com>
2022-06-22 13:32:23 +0200

    Freshdesk Tags + Folder Small Edit (#349)
    Co-authored-by: Diego Camelo <diego@hubs.com>

---
+++ models/4_reporting/freshdesk/fact_freshdesk_tags.sql
@@ -0,0 +1,23 @@
+-- Freshdesk Tags
+
+-- A Frehsdesk ticket can have multiple tags (~3 max)
+-- in this model we explode tags into various rows per ID
+-- This has a many to one relationship with tickets, used directly in Looker
+
+with tickets as (
+    select id,
+           replace(replace(tags,'[',''), ']', '') as ctags
+    from {{ ref('freshdesk_tickets') }} as ft
+    where ft.tags is not null 
+        and ft.tags <> '[]' -- Empty tags are not null but have this symbol
+        and ft._is_latest -- Necessary filter to avoid duplicates
+), sequence as (
+  select 1 as n union all
+  select 2 union all
+  select 3
+)
+select
+       tickets.id as ticket_id,
+       TRIM(SPLIT_PART(tickets.ctags, ',', seq.n)) as "tag"
+from sequence as seq
+inner join tickets ON seq.n <= REGEXP_COUNT(tickets.ctags, ',') + 1
