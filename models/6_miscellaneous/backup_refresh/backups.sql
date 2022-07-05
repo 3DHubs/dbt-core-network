cfaa9568eeb212e9a103d7d7bb3cf1b481b5ed2d
Daniel Salazar Soplapuco <85174516+Daniel-SalazarSoplapuco@users.noreply.github.com>
2022-07-05 16:48:19 +0200

    Ace 1083 dbt tests (#360)
    * Testing exclusion of dbt backups from dbt run
    
    * correction
    
    * Creating selectors
    
    * correction

--- models/6_miscellaneous/backup_refresh/backups.sql
+++ models/6_miscellaneous/backup_refresh/backups.sql
@@ -5,9 +5,11 @@
                 drop table if exists {{ source('dbt_backups', 'backup_fact_contribution_margin') }};
                 Create table {{ source('dbt_backups', 'backup_fact_contribution_margin') }} as
                 SELECT getdate() as backup_date, * FROM dbt_prod_reporting.fact_contribution_margin;
+
                 drop table if exists {{ source('dbt_backups', 'backup_fact_orders') }};
                 Create table {{ source('dbt_backups', 'backup_fact_orders') }} as
-                SELECT getdate() as backup_date, * FROM dbt_prod_reporting.fact_contribution_margin;
+                SELECT getdate() as backup_date, * FROM dbt_prod_reporting.fact_orders;
+                
                 drop table if exists {{ source('dbt_backups', 'backup_full_order_history_events') }};
                 Create table {{ source('dbt_backups', 'backup_full_order_history_events') }} as
                 SELECT getdate() as backup_date, * FROM data_lake.full_order_history_events
