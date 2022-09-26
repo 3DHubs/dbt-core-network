8c85fe016e051debe5182dddfceda9dd6999a37d
Daniel Salazar Soplapuco <85174516+Daniel-SalazarSoplapuco@users.noreply.github.com>
2022-09-26 13:56:02 +0200

    Ace 1133 exchange rate fluctuations (#390)
    * Adjusted the integrity test for contribution margin
    
    * jinja second test

--- models/6_miscellaneous/backup_tests/test.sql
+++ models/6_miscellaneous/backup_tests/test.sql
@@ -1,6 +1,5 @@
-{%- set default_schema = target.schema -%}
 
-{%- if default_schema == 'dbt_core'-%}
+{%- if target.name == 'dev' -%}
     select  'production' as schema_last_run
 {%- else -%}
     select  'personal' as schema_last_run
