e7a38737a1eb9a96a9e5f1db4272c60db1c429bd
Daniel Salazar Soplapuco <85174516+Daniel-SalazarSoplapuco@users.noreply.github.com>
2022-10-07 10:21:53 +0200

    fixing strategic flag removal issues (#397)

--- models/4_reporting/suppliers/dim_supplier_technologies.sql
+++ models/4_reporting/suppliers/dim_supplier_technologies.sql
@@ -5,9 +5,7 @@ select sup.id                                                 as supplier_id,
        ((supt.min_order_amount::float) / 100)::decimal(15, 2) as min_order_amount_usd,
        ((supt.max_order_amount::float) / 100)::decimal(15, 2) as max_order_amount_usd,
        supt.max_active_orders,
-       supt.allow_orders_with_custom_finishes,
-       supt.allow_strategic_orders,
-       supt.allow_non_strategic_orders
+       supt.allow_orders_with_custom_finishes
 from {{ ref('suppliers') }} as sup
             left outer join {{ ref('supplier_technologies') }} as supt on sup.id = supt.supplier_id
             left outer join {{ ref('technologies') }} as tec on supt.technology_id = tec.technology_id
