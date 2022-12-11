692fd1f6a360f10e8b1169826ba49dd1d66974aa
Daniel Salazar Soplapuco <85174516+Daniel-SalazarSoplapuco@users.noreply.github.com>
2022-12-11 22:45:55 +0100

    Allowing both the quote and order number to be used as input to get the order uuid. With the intention of recuding mistakes when order number was given by logistics but only quote is accepted. (#430)

--- models/4_reporting/logistics/fact_shipping_information.sql
+++ models/4_reporting/logistics/fact_shipping_information.sql
@@ -2,33 +2,33 @@
     tags=["adhoc"]
 ) }}
 
-
 select
-    oqsl.order_uuid,
-    sci.carrier,
-    sci.invoice_date,
-    sci.shipment_date,
-    sci.product_name,
-    sci.service_category,
-    sci.invoice_number,
-    sci.shipment_number,
-    sci.corporate_country,
-    sci.document_number,
-    sci.weight_kg,
-    sci.recipient_postal_code,
-    sci.shipper_postal_code,
-    coalesce(round(sci.statistical_value/ ex.rate,2),0) as statistical_value,
-    coalesce(ship.shipping_leg, sci.shipping_leg) as shipping_leg_full,
-    sci.charge_name,
-    sci.charge_category,
-    sci.currency,
-    sci.charge_amount as source_currency_charge_amount,
-    sci.total_amount as source_currency_total_amount,
-    coalesce(round(sci.charge_amount / ex.rate,2),0) as charge_amount,
-    coalesce(round(sci.total_amount / ex.rate,2),0) as total_costs_usd
-from {{ source('int_logistics', 'automated_shipping_customs_information') }} as sci
-left join {{ ref('prep_supply_documents') }}  as oqsl on sci.document_number = oqsl.document_number and oqsl.type = 'quote'
-left join {{ ref('prep_supply_integration') }}  as integration on integration.document_number = sci.document_number 
-left join {{ source('int_service_supply', 'shipments') }} as ship on ship.tracking_number = sci.shipment_number
-left join {{ source('data_lake', 'exchange_rate_spot_daily') }} as ex on sci.currency = ex.currency_code_to and sci.invoice_date = trunc(ex.date)
+    coalesce(psd.order_uuid, pso.uuid) as order_uuid,
+    asci.carrier,
+    asci.invoice_date,
+    asci.shipment_date,
+    asci.product_name,
+    asci.service_category,
+    asci.invoice_number,
+    asci.shipment_number,
+    asci.corporate_country,
+    asci.document_number,
+    asci.weight_kg,
+    asci.recipient_postal_code,
+    asci.shipper_postal_code,
+    coalesce(round(asci.statistical_value/ ex.rate,2),0) as statistical_value,
+    coalesce(ship.shipping_leg, asci.shipping_leg) as shipping_leg_full,
+    asci.charge_name,
+    asci.charge_category,
+    asci.currency,
+    asci.charge_amount as source_currency_charge_amount,
+    asci.total_amount as source_currency_total_amount,
+    coalesce(round(asci.charge_amount / ex.rate,2),0) as charge_amount,
+    coalesce(round(asci.total_amount / ex.rate,2),0) as total_costs_usd
+from {{ source('int_logistics', 'automated_shipping_customs_information') }} as asci
+left join {{ ref('prep_supply_documents') }} as psd on asci.document_number = psd.document_number
+left join {{ ref('prep_supply_orders') }} as pso on asci.document_number = pso.number
+left join {{ source('int_service_supply', 'shipments') }} as ship on ship.tracking_number = asci.shipment_number
+left join {{ source('data_lake', 'exchange_rate_spot_daily') }} as ex on asci.currency = ex.currency_code_to and asci.invoice_date = trunc(ex.date)
+left join {{ ref('prep_supply_integration') }} as integration on  coalesce(psd.order_uuid, pso.uuid) = integration.order_uuid
 where integration.is_test is not true
