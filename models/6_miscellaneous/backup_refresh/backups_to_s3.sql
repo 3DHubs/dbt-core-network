417af883c01a0718ffa904a12b48e37c3e148461
Daniel Salazar Soplapuco <85174516+Daniel-SalazarSoplapuco@users.noreply.github.com>
2023-02-14 14:53:44 +0100

    Fix selectors (#479)
    * changes in recognition logic
    
    * removing purchase order logic from stg_logistics
    
    * Testing selectors
    
    * Improved selectors
    
    * Removing faulty file
    
    * create test script for selectors to implement into macro
    
    * macro improvements
    
    * FInal fix on selectors and back up scripts + improved code efficiency
    
    * Fixing mistake
    
    * Only allow back ups to run when explicitly mentioned
    
    * fixing backups

--- models/6_miscellaneous/backup_refresh/backups_to_s3.sql
+++ models/6_miscellaneous/backup_refresh/backups_to_s3.sql
@@ -1,9 +1,9 @@
 {{ config(
     post_hook=["
-        {{unload_backups_to_s3('dbt_backups', 'fact_contribution_margin', True)}};
-        {{unload_backups_to_s3('dbt_backups', 'fact_orders', True)}};
-        {{unload_backups_to_s3('dbt_backups', 'dim_companies', True)}};
-        {{unload_backups_to_s3('dbt_backups', 'dim_contacts', True)}};                
+        {{unload_backups_to_s3('dbt_prod_reporting', 'fact_contribution_margin', True)}};
+        {{unload_backups_to_s3('dbt_prod_reporting', 'fact_orders', True)}};
+        {{unload_backups_to_s3('dbt_prod_reporting', 'dim_companies', True)}};
+        {{unload_backups_to_s3('dbt_prod_reporting', 'dim_contacts', True)}};                
         {{unload_backups_to_s3('data_lake', 'full_order_history_events', False)}}
     "],
 ) }}
