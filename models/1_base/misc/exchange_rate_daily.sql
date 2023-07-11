b274b10c73556f1a97406a2979bc0ef826faf804
Jurienat3dhubs <61703970+Jurienat3dhubs@users.noreply.github.com>
2023-07-11 17:50:22 +0200

    Update exchange_rate_daily.sql (#597)

--- models/1_base/misc/exchange_rate_daily.sql
+++ models/1_base/misc/exchange_rate_daily.sql
@@ -1,22 +1,21 @@
 with exchange_date_max as (select max(date) as max_date
                            from {{ source('data_lake', 'exchange_rate_spot_daily') }})
-select id,
-       date_add('day',1,date) as date,
+select date_add('day',1,date) as date,
        currency_code_base,
        currency_code_to,
-       rate,
        is_success,
-       is_historical
+       is_historical,
+       avg(rate) rate
 from {{ source('data_lake', 'exchange_rate_spot_daily') }}
 where date = (select max_date from exchange_date_max)
+group by 1,2,3,4,5
 union all
-select id,
-       date,
+select date,
        currency_code_base,
        currency_code_to,
-       rate,
        is_success,
-       is_historical
+       is_historical,
+       avg(rate) rate
 from {{ source('data_lake', 'exchange_rate_spot_daily') }}
-
+group by 1,2,3,4,5
 order by date desc
