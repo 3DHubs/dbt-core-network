1102b2c118df2437b535e3ddcc85369c5102969d
Jurienat3dhubs <61703970+Jurienat3dhubs@users.noreply.github.com>
2023-08-10 15:29:48 +0200

    Google conversion value fix (#612)
    * Fix try for Google Conversion Values
    
    * Update stg_mql_clv.sql

--- models/3_staging/contacts/stg_mql_clv.sql
+++ models/3_staging/contacts/stg_mql_clv.sql
@@ -152,6 +152,6 @@ left join
     on clv.first_closed_order_technology = dc.mql_technology
     and clv.region = dc.region
     and clv.cohort = 1
-    and became_mql_at >= '2021-01-01'
+    and became_mql_at >= '2021-01-01' and became_mql_at < date_add('days',-2,getdate())
 
 order by 1
