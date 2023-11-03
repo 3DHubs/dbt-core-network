94ba6b757b15f84d9078be26847c89c99c7ad43c
Clara G <108463154+clarahubs@users.noreply.github.com>
2023-11-03 09:38:56 +0100

    Sales owner repository (#667)
    * Sales repository for previous sales reps
    
    * fixed owner_id alias
    
    * excluding on hubspot_id

--- models/1_base/hubspot/hubspot_owners.sql
+++ models/1_base/hubspot/hubspot_owners.sql
@@ -11,10 +11,19 @@ with employees as ( --multiple rows are inserted in source, distinction not clea
     last_name,
     location
    from {{ source('ext_gsheets', 'hr_bamboo_employees') }}
+),
+sales_hubspot_ids as(
+    select
+    hubspot_id,
+    firstname,
+    lastname,
+    fullname,
+    email
+    from {{ ref('seed_hs_id_sales_team') }} 
 )
 select
 	   owner_id,
-       ho.email,
+       ho.email as email,
        ho.first_name,
        ho.last_name,
        name,
@@ -24,8 +33,38 @@ select
        archived,
        loaded_at,
        primary_team_name,
-       is_current,
+      is_current,
 	   coalesce(location,'Amsterdam') as location,
-       case when location <> 'Chicago' then 'Amsterdam' else 'Chicago' end as office_location 
+      case when location <> 'Chicago' then 'Amsterdam' else 'Chicago' end as office_location 
 	from {{ ref('hubspot_owners_base') }} ho 
 	left join employees hr on (ho.first_name = hr.first_name and ho.last_name = hr.last_name) or hr.email = ho.email
+--     WHERE (ho.first_name, ho.last_name) NOT IN (
+--     SELECT firstname, lastname
+--     FROM sales_hubspot_ids
+-- )
+  
+
+ union all
+select 
+       hubspot_id as owner_id,
+       hsid.email as email,
+       firstname as first_name,
+       lastname as last_name,
+       fullname as name,
+       null as user_id,
+       null as created_at,
+       null as updated_at,
+       null as archived,
+       null as loaded_at,
+       null as primary_team_name,
+       null as is_current,
+	   null as location,
+      null as office_location
+    from sales_hubspot_ids hsid
+    WHERE (hsid.hubspot_id) NOT IN (
+    SELECT owner_id
+    FROM {{ ref('hubspot_owners_base') }}
+    )
+    order by 1
+    
+    
