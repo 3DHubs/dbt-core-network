d2a150e8063a2dc1e76ac4a306b32defd6358259
Jurienat3dhubs <61703970+Jurienat3dhubs@users.noreply.github.com>
2023-12-27 15:46:51 +0100

    count unique auctions (#701)
    * cancellation fix

--- models/3_staging/orders/agg_orders_auctions.sql
+++ models/3_staging/orders/agg_orders_auctions.sql
@@ -2,7 +2,8 @@ select
     pa.order_uuid,
     bool_or(case when pa.winning_bid_uuid is not null then true else false end) has_winning_bid_any_auction, -- If any of the auction on an order had a winning bid
     sum(case when pb.supplier_id != coalesce(sod.po_active_supplier_id,-1) then 1 else 0 end) as number_of_auction_cancellations,
-    count(1) as number_of_auctions
+    count(distinct case when auction_type = 'RFQ' then pa.quote_uuid else pa.auction_uuid  end) as number_of_auctions
+
     from dbt_prod_core.prep_auctions pa
          left join {{ ref('prep_bids') }} pb on pb.uuid = pa.winning_bid_uuid
          left join {{ ref('stg_orders_documents') }} sod on sod.order_uuid = pa.order_uuid
