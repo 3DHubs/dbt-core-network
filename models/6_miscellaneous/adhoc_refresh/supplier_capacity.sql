18c5d8345b53cea4d0b15cdb956e0d5a14b1d421
Jurienat3dhubs <61703970+Jurienat3dhubs@users.noreply.github.com>
2024-03-14 12:45:37 +0100

    Additional budget lines (#744)
    * budget_additions

---
+++ models/6_miscellaneous/adhoc_refresh/supplier_capacity.sql
@@ -0,0 +1,40 @@
+                select fo.supplier_id,
+                       d.date                           as production_at,
+                       sum(subtotal_sourced_amount_usd) as subtotal_sourced_amount_usd,
+                       count(1)                         as orders,
+                       null as seen_rate,
+                       null as seen_volume,
+                       null as positive_response_rate,
+                       null as positive_volume
+                from dbt_prod_reporting.fact_orders fo
+                         inner join
+                     int_analytics.dim_dates d
+                     on fo.sourced_at <= d.date
+                         and coalesce(fo.order_shipped_at,promised_shipping_at_by_supplier) > d.date
+                         and order_status != 'canceled'
+                group by 1,2
+                union all
+                select sa_supplier_id                                                                             as supplier_id,
+                       fact_rda_behaviour.sa_assigned_at::date,
+                       null as subtotal_sourced_amount_usd,
+                       null as orders,
+                       (COUNT(CASE
+                                  WHEN (fact_rda_behaviour.sa_first_seen_at IS NOT NULL) THEN fact_rda_behaviour.sa_uuid
+                                  ELSE NULL END)) * 1.0 /
+                       nullif((COUNT(DISTINCT fact_rda_behaviour.sa_uuid)), 0)                                    as seen_rate,
+                       COUNT(CASE
+                                 WHEN (fact_rda_behaviour.sa_first_seen_at IS NOT NULL) THEN fact_rda_behaviour.sa_uuid
+                                 ELSE NULL END)                                                                   as seen_volume,
+                       (COUNT(CASE
+                                  WHEN (fact_rda_behaviour.response_type IN ('accepted', 'countered'))
+                                      THEN fact_rda_behaviour.sa_uuid
+                                  ELSE NULL END)) * 1.0 /
+                       nullif((COUNT(DISTINCT fact_rda_behaviour.sa_uuid)), 0)                                    as positive_response_rate,
+                       (COUNT(CASE
+                                  WHEN (fact_rda_behaviour.response_type IN ('accepted', 'countered'))
+                                      THEN fact_rda_behaviour.sa_uuid
+                                  ELSE NULL END))                                                                 as positive_volume
+
+                FROM dbt_prod_reporting.fact_auction_behaviour AS fact_rda_behaviour
+                WHERE ((fact_rda_behaviour.is_rfq = 'false'))
+                group by 1, 2
