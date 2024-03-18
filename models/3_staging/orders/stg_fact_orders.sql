624acf0288d6a1c37059294b56833c123fff000c
Jurienat3dhubs <61703970+Jurienat3dhubs@users.noreply.github.com>
2024-03-18 11:25:02 +0100

    Fix precalc and remove budget duplicates (#751)
    * Fix precalc exceptions

--- models/3_staging/orders/stg_fact_orders.sql
+++ models/3_staging/orders/stg_fact_orders.sql
@@ -532,12 +532,13 @@ select
     case when is_closed then subtotal_amount_usd else 0 end                                as subtotal_closed_amount_usd,
     case when is_sourced then subtotal_amount_usd else 0 end                               as subtotal_sourced_amount_usd,
     case when is_logistics_shipping_quote_used = false and qli.line_item_technology_name = '3DP' then subtotal_amount_usd *1.0 * 0.03 
+         when rda.is_first_auction_rda_sourced = false and is_cross_docking = false then 0
          else qli.shipping_amount_usd end                                                  as prep_shipping_cost_usd, 
-    coalesce(case when rda.is_first_auction_rda_sourced then rda.first_winning_shipping_estimate_amount_usd end,0) + coalesce(prep_shipping_cost_usd,0)  as shipping_cost_usd,
+    case when is_sourced then coalesce(case when rda.is_first_auction_rda_sourced then rda.first_winning_shipping_estimate_amount_usd end,0) + coalesce(prep_shipping_cost_usd,0) else 0 end  as shipping_cost_usd,
     case when po_production_finalized_at < coalesce(logistics.shipped_at,'2100-01-01') and coalesce(auc.has_winning_bid_any_auction, false) = false  then po_production_subtotal_cost_usd
           else  po_first_sourced_cost_usd  end                                             as subtotal_po_cost_usd,    
-    coalesce(subtotal_po_cost_usd,0) + coalesce(shipping_cost_usd,0) + 
-    estimated_l1_customs_amount_usd + estimated_l2_customs_amount_usd as subtotal_sourced_cost_usd,
+    case when is_sourced then coalesce(subtotal_po_cost_usd,0) + coalesce(shipping_cost_usd,0) + 
+    estimated_l1_customs_amount_usd + estimated_l2_customs_amount_usd else 0 end as subtotal_sourced_cost_usd,
 
     -- Suppliers:
     coalesce(docs.po_active_supplier_id, rda.supplier_id)                          as supplier_id,
