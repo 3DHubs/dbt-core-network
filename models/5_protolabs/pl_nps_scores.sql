26086e511fb200ea55938d30f21df995ad7cfbea
Jurienat3dhubs <61703970+Jurienat3dhubs@users.noreply.github.com>
2024-04-09 16:18:46 +0200

    NPS additional field (#766)
    * nps comments

--- models/5_protolabs/pl_nps_scores.sql
+++ models/5_protolabs/pl_nps_scores.sql
@@ -1,5 +1,6 @@
 select rating,
        order_id as order_uuid,
-       submit_date as submit_at
+       submit_date as submit_at,
+       additional_comments
 from {{ source('int_analytics', 'nps_scores') }}
 where submit_date >= date_add('years',-2,date_trunc('year',getdate()))
