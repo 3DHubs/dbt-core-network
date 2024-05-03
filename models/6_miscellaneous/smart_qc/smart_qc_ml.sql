e0999974e69335fb3c0b525d11865bb020565c25
Jurienat3dhubs <61703970+Jurienat3dhubs@users.noreply.github.com>
2024-05-03 09:52:20 +0200

    Multi refresh smart qc ml (#787)
    * multi refresh

--- models/6_miscellaneous/smart_qc/smart_qc_ml.sql
+++ models/6_miscellaneous/smart_qc/smart_qc_ml.sql
@@ -1,9 +1,11 @@
-{{ config(materialized="incremental") }}
+{{ config(materialized="incremental"
+, tags=["multirefresh"]) }}
+
 -- This model makes sure that the upsert int_analytics.smart_qc only stores the result the moment the shipped at is known.
 select distinct qc.line_item_uuid, 
        qc.predicted_proba,
        qc.model_executed_at
-    from {{ source("int_analytics", "smart_qc") }} qc
+    from {{ source("int_analytics", "smart_qc") }} qc   
     inner join {{ ref("prep_line_items") }} li on li.uuid = qc.line_item_uuid
     inner join {{ ref("stg_orders_logistics") }}  fo on fo.order_uuid = li.order_uuid
     where fo.shipped_at is not null
