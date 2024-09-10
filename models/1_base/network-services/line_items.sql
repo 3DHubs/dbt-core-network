8c8a69f082b21fc34c7674545a57fc45420b5bac
annabogo-hubs <132448207+annabogo-hubs@users.noreply.github.com>
2024-09-10 14:14:03 +0200

    Add line_items from gold_line_items in network services (#847)
    * added gold_line_items ref
    
    * added line_items to network_service
    
    * test commit
    
    * fixed the list of deleted columns
    
    * added schema_changes config to LI

---
+++ models/1_base/network-services/line_items.sql
@@ -0,0 +1,91 @@
+{{
+    config(
+        materialized='incremental',
+        on_schema_change='sync_all_columns',
+        unique_key='uuid',
+        tags=["multirefresh"],
+        post_hook=["delete from {{ this }}  where uuid not in (select uuid from {{ source('int_service_supply', 'line_items') }} )"]
+    )
+}}
+
+
+select  
+       created,
+       updated as li_updated_at,
+       deleted,
+       id,
+       uuid,
+       quote_uuid,
+       correlation_uuid,
+       upload_id,
+       type,
+       quantity,
+       auto_price_original_amount,
+       price_amount,
+       unit,
+       custom_material_subset_name,
+       finish_slug,
+       custom_finish_name,
+       title,
+       description,
+       admin_description,
+       auto_price_amount,
+       unit_price_amount,
+       material_subset_id,
+       material_id,
+       process_id,
+       technology_id,
+       parent_uuid,
+       auto_tooling_price_amount,
+       auto_tooling_price_original_amount,
+       tooling_price_amount,
+       infill,
+       layer_height,
+       material_color_id,
+       lead_time_options,
+       legacy_id,
+       branded_material_id,
+       part_orientation_additional_notes,
+       part_orientation_vector,
+       weight_in_grams,
+       tax_code,
+       tax_details,
+       tax_price_amount,
+       tax_price_exempt_amount,
+       upload_properties,
+       price_multiplier,
+       commodity_code,
+       shipping_option_id,
+       target_margin,
+       commodity_code_source,
+       discount_id,
+       discount_code_id,
+       tolerance_id,
+       custom_tolerance,
+       custom_tolerance_unit,
+       general_tolerance_class,
+       estimated_first_leg_customs_amount_usd,
+       estimated_second_leg_customs_amount_usd,
+       estimated_price_amount,
+       estimated_price_original_amount,
+       thickness,
+       exceeds_standard_tolerances,
+       has_threads,
+       has_fits,
+       has_internal_corners,
+       has_part_marking,
+       should_quote_manually,
+       has_technical_drawings,
+       is_visible_to_supplier,
+       is_cosmetic
+
+from {{ ref('gold_line_items') }} as li
+where true
+    and legacy_id is null
+    
+    {% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+    and (updated >= (select max(li_updated_at) from {{ this }}) )
+
+    {% endif %}
