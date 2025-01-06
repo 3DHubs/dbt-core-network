f82da779cc166ebe0169739881876ea800b85c97
Jurienat3dhubs <61703970+Jurienat3dhubs@users.noreply.github.com>
2025-01-06 15:18:18 +0100

    Smart QC improvement history (#916)
    * improve smart QC

--- models/6_miscellaneous/smart_qc/smart_qc_ml.sql
+++ models/6_miscellaneous/smart_qc/smart_qc_ml.sql
@@ -1,5 +1,20 @@
 {{ config(materialized="incremental"
 , tags=["multirefresh"]
+      ,pre_hook=[""" {% if  env_var('DBT_ENVIRONMENT') == 'prod' %} 
+            INSERT INTO {{ source('int_analytics', 'smart_qc_unique_history') }}
+            SELECT 
+            qc.line_item_uuid, 
+            qc.model_executed_at, 
+            qc.predicted_proba
+            FROM {{ source('int_analytics', 'smart_qc') }} as qc
+            inner join {{ ref('prep_line_items') }} li on li.uuid = qc.line_item_uuid
+            inner join {{ ref('stg_orders_logistics') }}  fo on fo.order_uuid = li.order_uuid
+            where fo.shipped_at is not null
+            and line_item_uuid NOT IN (
+                SELECT
+                    line_item_uuid
+            FROM {{ source('int_analytics', 'smart_qc_unique_history') }})
+               {% endif %} """]
 
     ,post_hook = """ {% if  env_var('DBT_ENVIRONMENT') == 'prod' %} 
             unload ('SELECT line_item_uuid, predicted_proba FROM  {{ this }} where loaded_at = \\'{{ run_started_at }}\\' ') 
@@ -14,14 +29,12 @@
 
 
 -- This model makes sure that the upsert int_analytics.smart_qc only stores the result the moment the shipped at is known.
-select distinct qc.line_item_uuid, 
-       qc.predicted_proba,
-       qc.model_executed_at,
+select line_item_uuid, 
+       predicted_proba,
+       model_executed_at,
        '{{ run_started_at }}' as loaded_at
-    from {{ source("int_analytics", "smart_qc") }} qc   
-    inner join {{ ref("prep_line_items") }} li on li.uuid = qc.line_item_uuid
-    inner join {{ ref("stg_orders_logistics") }}  fo on fo.order_uuid = li.order_uuid
-    where fo.shipped_at is not null
+    from {{ source("int_analytics", "smart_qc_unique_history") }} qc   
+    where true
    
       {% if is_incremental() %}
 
