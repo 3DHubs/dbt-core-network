a9644b1098acfa6d5e57e354c600235cdca30765
Jurienat3dhubs <61703970+Jurienat3dhubs@users.noreply.github.com>
2025-02-05 21:20:51 +0100

    Leverage commodity Snap (#931)
    * customs

--- models/3_staging/lineitems/stg_line_items_customs.sql
+++ models/3_staging/lineitems/stg_line_items_customs.sql
@@ -1,9 +1,12 @@
 select li.uuid, 
-        nvl(cc_rates.rate,0) as estimated_l1_customs_rate
+        nvl(snapcc.rate,0) as estimated_l1_customs_rate
         
         from {{ ref('prep_line_items')}} as li
         inner join {{ ref('prep_purchase_orders')}} po on (li.quote_uuid = po.uuid)
         inner join {{ ref('suppliers')}} s on (po.supplier_id = s.id)
-        inner join  {{ ref('network_services', 'gold_commodity_code_customs_rates') }}  cc_rates on (li.commodity_code = cc_rates.commodity_code and cc_rates.shipping_country_id = po.shipping_country_id and cc_rates.supplier_country_id=s.country_id)
+        inner join  {{ ref('snap_commodity_code') }}  snapcc on (li.commodity_code = snapcc.commodity_code and snapcc.shipping_country_id = po.shipping_country_id
+                    and snapcc.supplier_country_id=s.country_id and  po.created >= snapcc.dbt_valid_from and po.created < coalesce(snapcc.dbt_valid_to, getdate()))
         where true
         and type = 'part'
+
+       
