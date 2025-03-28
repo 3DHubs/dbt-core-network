1411d453160a90e7b1747e8e9432a1c2058bcab8
aleckevinhubs <135629263+aleckevinhubs@users.noreply.github.com>
2025-03-28 10:04:54 +0100

    Alec ace 1935 robowarehouse uk (#948)
    * robowarehouse uk
    
    * ukrobowarehouse
    
    * uk robowarehouse

--- models/1_base/retool/logistics_operation.sql
+++ models/1_base/retool/logistics_operation.sql
@@ -66,3 +66,39 @@ select
     
 from
     {{ source('int_retool', 'ups_api_documents') }} as us_logistics_operation
+
+union all 
+
+select
+    'uk'                                                                                                     as robowarehouse_region,
+    md5(concat('uk_', id))                                                                                   as logistics_operation_uuid,
+    id                                                                                                       as logistics_operation_id,
+    tracking_number,
+    ordnum                                                                                                   as document_number,
+    cast(piece_count as integer)                                                                             as package_piece_count,
+    coalesce(round(cast(expected_shipping_costs as double precision) / ex.rate, 2), 0)                       as expected_shipping_costs_usd,
+    service_level,
+    package_handler,
+    destination_post_code                                                                                    as destination_code,
+    destination_company,
+    null                                                                                                     as destination_state_code,
+    null                                                                                                     as destination_zip_code,
+    coalesce(
+    case when total_weight ~ '^[0-9]+(\.[0-9]+)?$' then cast(total_weight as numeric)
+    else null end, 0)                                                                                        as package_total_weight_kg,  -- Original weight in kg
+    coalesce(
+    case when total_weight ~ '^[0-9]+(\.[0-9]+)?$' then cast(total_weight as numeric) * 2.20462
+    else null end, 0)                                                                                        as package_total_weight_lb,  -- Convert to lb
+
+    -- Timestamp Fields
+    to_timestamp(replace(replace(started_at, 'T', ' '), 'Z', ''), 'YYYY-MM-DD HH:MI:SS.MS')::timestamp       as logistics_operation_start_time,
+    created_at                                                                                               as label_created_at,
+    null                                                                                                     as logistics_operation_updated_at,
+    extract(epoch from (label_created_at - logistics_operation_start_time))                                  as time_spent_in_seconds,
+
+    --Boolean Fields
+    {{ varchar_to_boolean('is_conforming') }}
+
+from 
+    {{ source('int_retool', 'uk_ups_api_documents') }} as uk_logistics_operation
+    left join {{ ref('exchange_rate_daily') }} as ex on uk_logistics_operation.expected_shipping_cost_currency = ex.currency_code_to and trunc(uk_logistics_operation.created_at) = trunc(ex.date)
