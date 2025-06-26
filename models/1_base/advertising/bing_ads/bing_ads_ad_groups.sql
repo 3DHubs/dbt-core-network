4b1c72bf17f22f2f912624b0bdb0380c6bf2e9e1
Diego Camelo <diego@3dhubs.com>
2025-06-26 15:40:44 +0200

    reverted changes in bing models

--- models/1_base/advertising/bing_ads/bing_ads_ad_groups.sql
+++ models/1_base/advertising/bing_ads/bing_ads_ad_groups.sql
@@ -1,8 +1,9 @@
 select id, name, 'network_amer' as source
 from {{ source('ext_bing', 'ad_groups') }}
 union all
-select id, name, 'factory' as source
-from {{ source('ext_bing_emea', 'ad_groups') }}
-union all
+-- todo-migration: the table below is not available yet, add when available
+-- select id, name, 'factory' as source
+-- from {{ source('_ext_bing_factory', 'ad_groups') }}
+-- union all
 select id, name, 'network_emea' as source
 from {{ source('ext_bing_emea', 'ad_groups') }}
--- models/1_base/advertising/bing_ads/bing_ads_keywords_source.sql
+++ models/1_base/advertising/bing_ads/bing_ads_keywords_source.sql
@@ -21,29 +21,30 @@ select
     'network_amer' as source
 from {{ source('ext_bing', 'keyword_performance_report') }}
 union all
-select
-    __sdc_primary_key,
-    _sdc_batched_at,
-    _sdc_received_at,
-    _sdc_report_datetime,
-    _sdc_sequence,
-    _sdc_table_version,
-    accountid,
-    accountname,
-    adgroupid,
-    adgroupname,
-    campaignid,
-    campaignname,
-    clicks,
-    currencycode,
-    impressions,
-    keyword,
-    keywordid,
-    spend,
-    timeperiod,
-    'factory' as source
-from {{ source('ext_bing_emea', 'keyword_performance_report') }}
-union all
+-- todo-migration: the table below is not available yet, add when available
+-- select
+--     __sdc_primary_key,
+--     _sdc_batched_at,
+--     _sdc_received_at,
+--     _sdc_report_datetime,
+--     _sdc_sequence,
+--     _sdc_table_version,
+--     accountid,
+--     accountname,
+--     adgroupid,
+--     adgroupname,
+--     campaignid,
+--     campaignname,
+--     clicks,
+--     currencycode,
+--     impressions,
+--     keyword,
+--     keywordid,
+--     spend,
+--     timeperiod,
+--     'factory' as source
+-- from {{ source('_ext_bing_factory', 'keyword_performance_report') }}
+-- union all
 select
     __sdc_primary_key,
     _sdc_batched_at,
