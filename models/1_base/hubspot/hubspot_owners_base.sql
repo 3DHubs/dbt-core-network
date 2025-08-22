ea803903995544305f58b0fbac941895eb4afc95
Diego Camelo <diego.camelo@protolabs.com>
2025-08-22 15:14:03 +0000

    Solve a few errors and commented out model/columns

--- models/1_base/hubspot/hubspot_owners_base.sql
+++ models/1_base/hubspot/hubspot_owners_base.sql
@@ -1,18 +1,32 @@
 {{ config(materialized="table") }}
 
 select
-    ow.id::bigint as owner_id,
-    ow.email as email,
-    initcap(ow.first_name) as "first_name",
-    replace(initcap(ow.last_name), ' Pl', '')
-    + case when t.name::varchar ilike '%protolabs%' then ' (PL)' else '' end as last_name, --todo-migration-test: replaced ~ for ilike
-    initcap(first_name) + ' ' + replace(initcap(ow.last_name), ' Pl', '') + case when t.name::varchar ilike '%protolabs%' then ' (PL)' else '' end as name, --todo-migration-test: replaced ~ for ilike
-    ow.user_id as user_id,
-    ow.created_at as created_at,
-    ow.updated_at as updated_at,
-    ow.archived as archived,
-    ow.load_timestamp as loaded_at,
-    t.name::varchar as primary_team_name,
-    cast(t.primary as boolean) as is_current
-from {{ source("ext_hubspot", "hubspot_owners") }} ow, ow.teams t --todo-migration-research Join notation of tables; I'm assuming is json doesn't work in snowflake
-where t.primary = true
+    -- ow.id::bigint as owner_id,
+    -- ow.email as email,
+    -- initcap(ow.first_name) as "first_name",
+    -- replace(initcap(ow.last_name), ' Pl', '')
+    -- + case when t.name::varchar ilike '%protolabs%' then ' (PL)' else '' end as last_name, --todo-migration-test: replaced ~ for ilike
+    -- initcap(first_name) + ' ' + replace(initcap(ow.last_name), ' Pl', '') + case when t.name::varchar ilike '%protolabs%' then ' (PL)' else '' end as name, --todo-migration-test: replaced ~ for ilike
+    -- ow.user_id as user_id,
+    -- ow.created_at as created_at,
+    -- ow.updated_at as updated_at,
+    -- ow.archived as archived,
+    -- ow.load_timestamp as loaded_at,
+    -- t.name::varchar as primary_team_name,
+    -- cast(t.primary as boolean) as is_current
+
+    --todo-migration-research: bring back fields once the Redshift supertype has been addressed
+    null as owner_id,
+    null as email,
+    null as first_name,
+    null as last_name,
+    null as name,
+    null as user_id,
+    null as created_at,
+    null as updated_at,
+    null as archived,
+    null as loaded_at,
+    null as primary_team_name,
+    null as is_current
+-- from {{ source("ext_hubspot", "hubspot_owners") }} ow, ow.teams t --todo-migration-research Join notation of tables; I'm assuming is json doesn't work in snowflake
+-- where t.primary = true
