77aa9b4b9801cf441e74f5e2d966cb5a34fa18f3
Diego Camelo <diego.camelo@protolabs.com>
2025-08-28 12:06:42 +0000

    Fixing hubspot_owners_base

--- models/1_base/hubspot/hubspot_owners_base.sql
+++ models/1_base/hubspot/hubspot_owners_base.sql
@@ -1,32 +1,37 @@
 {{ config(materialized="table") }}
 
-select
-    -- ow.id::bigint as owner_id,
-    -- ow.email as email,
-    -- initcap(ow.first_name) as "first_name",
-    -- replace(initcap(ow.last_name), ' Pl', '')
-    -- + case when t.name::varchar ilike '%protolabs%' then ' (PL)' else '' end as last_name, --todo-migration-test: replaced ~ for ilike
-    -- initcap(first_name) + ' ' + replace(initcap(ow.last_name), ' Pl', '') + case when t.name::varchar ilike '%protolabs%' then ' (PL)' else '' end as name, --todo-migration-test: replaced ~ for ilike
-    -- ow.user_id as user_id,
-    -- ow.created_at as created_at,
-    -- ow.updated_at as updated_at,
-    -- ow.archived as archived,
-    -- ow.load_timestamp as loaded_at,
-    -- t.name::varchar as primary_team_name,
-    -- cast(t.primary as boolean) as is_current
+--todo-migration-test: refactored this model because of semi-structured
+--todo-migration-research: this might change when updating the ingestion method
 
-    --todo-migration-research: bring back fields once the Redshift supertype has been addressed
-    null as owner_id,
-    null as email,
-    null as first_name,
-    null as last_name,
-    null as name,
-    null as user_id,
-    null as created_at,
-    null as updated_at,
-    null as archived,
-    null as loaded_at,
-    null as primary_team_name,
-    true as is_current
--- from {{ source("ext_hubspot", "hubspot_owners") }} ow, ow.teams t --todo-migration-research Join notation of tables; I'm assuming is json doesn't work in snowflake
--- where t.primary = true
+with owners_flat as (
+  select
+      owners.id::bigint                  as owner_id,
+      owners.email                       as email,
+      initcap(owners.first_name)         as first_name,
+      initcap(owners.last_name)          as last_name_raw,        -- keep raw
+      team.value:name::string            as team_name,
+      team.value:primary::boolean        as is_current,
+      owners.user_id                     as user_id,
+      owners.created_at                  as created_at,
+      owners.updated_at                  as updated_at,
+      owners.archived                    as archived,
+      owners.load_timestamp              as loaded_at
+  from {{ source("ext_hubspot", "hubspot_owners") }} owners,
+       lateral flatten(input => parse_json(owners.teams)) team
+), final as (
+
+-- Simple: includes last_name_raw
+select
+    *,
+  replace(coalesce(last_name_raw,''), ' Pl', '') ||
+    case when team_name ilike '%protolabs%' then ' (PL)' else '' end as last_name,
+  concat_ws(' ',
+    first_name,
+    replace(coalesce(last_name_raw,''), ' Pl', '') ||
+      case when team_name ilike '%protolabs%' then ' (PL)' else '' end
+  ) as owner_name
+from owners_flat
+where is_current
+)
+select *
+from final
